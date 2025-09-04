0376409016b5fc54fa397efd34cd9a746826f969
Domgoz <celesfax@proton.me>
2025-09-04 17:47:14 +0200

    Date Done

--- mori.js
+++ mori.js
@@ -1,32 +1,50 @@
-
-
 function submitDate() {
     const dateValue = document.getElementById("dateDeath").value;
     if (dateValue) {
-        const d = new Date();
-        d.setFullYear(d.getFullYear() + 20);
-        const expires = "expires=" + d.toUTCString();
-        document.cookie = "dateOfDeath=" + dateValue + ";" + expires + ";path=/";
-        alert("Cookie set: dateOfDeath = " + dateValue);
-        console.log(dateValue);
-     } else {
-            alert("Please select a date.");
-        }
+        localStorage.setItem("dateOfDeath", dateValue);
+        alert("Date saved: dateOfDeath = " + dateValue);
+        window.location.replace("mori.html");
+    } else {
+        alert("Please select a date.");
+    }
+}
+
+function setUpCountdown(dateString) {
+    let countDownDate = dateString
+        ? new Date(dateString).getTime()
+        : new Date("Jan 5, 2030 15:37:25").getTime();
+
+    if (window.countdownInterval) {
+        clearInterval(window.countdownInterval);
     }
 
-var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+    window.countdownInterval = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
 
-var x = setInterval(function() {
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
+        var timesEl = document.getElementById("times");
+        if (timesEl) {
+            timesEl.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+            if (distance < 0) {
+                clearInterval(window.countdownInterval);
+                timesEl.innerHTML = "EXPIRED";
+            }
+        }
+    }, 1000);
+}
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    
-    document.getElementById("times").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-}, 1000);
-
+document.addEventListener("DOMContentLoaded", function() {
+    if (!localStorage.getItem("dateOfDeath") && !window.location.pathname.endsWith("setup.html"))  {
+        window.location.replace('setup.html');
+        return;
+    }
+    var submitBtn = document.getElementById("submitbtn");
+    if (submitBtn) submitBtn.addEventListener("click", submitDate);
+    const savedDate = localStorage.getItem("dateOfDeath");
+    setUpCountdown(savedDate);
+});
